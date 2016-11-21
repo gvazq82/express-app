d8ccd8c49f3de6d34d929c7f8fec410e9108ab7a
gvazquez <gvazquez@mediamorph.com>
2016-11-21 01:22:00 -0500

    Bluebird

--- models/todos.js
+++ models/todos.js
@@ -1,5 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Promise = require('bluebird'); //ADD THIS LINE
+Promise.promisifyAll(mongoose); //AND THIS LINE
+
 var TodoSchema = new Schema({
   text: {type: 'String', required: true},
   done: {type: 'Boolean'}
