591f1d65adc7155b3f0eab477d0410e7b7899a70
gvazquez <gvazquez@mediamorph.com>
2016-12-10 00:52:29 -0500

    Bootstrap

--- app.js
+++ app.js
@@ -61,6 +61,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/fonts', express.static(path.join(__dirname, 'node_modules/bootstrap-sass/assets/fonts'))); // ADD THIS
+
 
 app.use('/', index);
 app.use('/users', users);
